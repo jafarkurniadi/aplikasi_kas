2301ec13a990883c315d84fc3996fcd98c0d5427
jafarkurniadi <58169625+jafarkurniadi@users.noreply.github.com>
2019-12-04 22:50:44 +0800

    Add files via upload

---
+++ kas/kas/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.txtJumlah = New System.Windows.Forms.TextBox()
+        Me.txtKode = New System.Windows.Forms.TextBox()
+        Me.txtKet = New System.Windows.Forms.TextBox()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.lbl_Saldo = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!)
+        Me.Label1.Location = New System.Drawing.Point(234, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(334, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aplikasi Kas Keluar Masuk"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Kode Transaksi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(237, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Tanggal"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(444, 62)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Jenis"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(622, 62)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jumlah"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(48, 148)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(82, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Keterangan"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(210, 96)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(128, 22)
+        Me.DateTimePicker1.TabIndex = 6
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(416, 94)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'txtJumlah
+        '
+        Me.txtJumlah.Location = New System.Drawing.Point(598, 96)
+        Me.txtJumlah.Name = "txtJumlah"
+        Me.txtJumlah.Size = New System.Drawing.Size(100, 22)
+        Me.txtJumlah.TabIndex = 8
+        '
+        'txtKode
+        '
+        Me.txtKode.Location = New System.Drawing.Point(51, 96)
+        Me.txtKode.Name = "txtKode"
+        Me.txtKode.Size = New System.Drawing.Size(104, 22)
+        Me.txtKode.TabIndex = 9
+        '
+        'txtKet
+        '
+        Me.txtKet.Location = New System.Drawing.Point(169, 148)
+        Me.txtKet.Name = "txtKet"
+        Me.txtKet.Size = New System.Drawing.Size(563, 22)
+        Me.txtKet.TabIndex = 10
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(117, 191)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(108, 52)
+        Me.btnSimpan.TabIndex = 11
+        Me.btnSimpan.Text = "SIMPAN"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(253, 191)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(99, 52)
+        Me.btnEdit.TabIndex = 12
+        Me.btnEdit.Text = "EDIT"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(394, 191)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(121, 52)
+        Me.btnHapus.TabIndex = 13
+        Me.btnHapus.Text = "HAPUS"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 262)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(776, 176)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'lbl_Saldo
+        '
+        Me.lbl_Saldo.AutoSize = True
+        Me.lbl_Saldo.Location = New System.Drawing.Point(609, 191)
+        Me.lbl_Saldo.Name = "lbl_Saldo"
+        Me.lbl_Saldo.Size = New System.Drawing.Size(89, 17)
+        Me.lbl_Saldo.TabIndex = 15
+        Me.lbl_Saldo.Text = "Saldo Utama"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl_Saldo)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.txtKet)
+        Me.Controls.Add(Me.txtKode)
+        Me.Controls.Add(Me.txtJumlah)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents txtJumlah As TextBox
+    Friend WithEvents txtKode As TextBox
+    Friend WithEvents txtKet As TextBox
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents lbl_Saldo As Label
+End Class
---
+++ kas/kas/Form1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+    Public tampil As Integer
+    Public saldo As Integer
+
+    Sub TampilGrid()
+        bukakoneksi()
+
+        DA = New OdbcDataAdapter("select * from tbl_kas", CONN)
+        DS = New DataSet
+        DA.Fill(DS, "tbl_kas")
+        DataGridView1.DataSource = DS.Tables("tbl_kas")
+
+        tutupkoneksi()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TampilGrid()
+
+        ComboJenis()
+        updateDisplay()
+    End Sub
+
+    Sub ComboJenis()
+        ComboBox1.Items.Add("Masuk")
+        ComboBox1.Items.Add("Keluar")
+    End Sub
+
+
+    Sub KosongkanData()
+        txtKode.Text = ""
+        txtJumlah.Text = ""
+        txtKet.Text = ""
+        ComboBox1.Text = ""
+    End Sub
+
+    Private Sub BtnSimpan_Click(sender As Object, e As EventArgs) Handles btnSimpan.Click
+        If txtJumlah.Text = "" Or txtKet.Text = "" Then
+            MsgBox("Silahkan isi semua form")
+        Else
+            bukakoneksi()
+            If ComboBox1.Text = "Masuk" Then
+                saldo = CInt(txtJumlah.Text)
+                tampil = tampil + saldo
+            ElseIf ComboBox1.Text = "Keluar" Then
+                saldo = CInt(txtJumlah.Text)
+                tampil = tampil - saldo
+            End If
+
+            If tampil < 0 Then
+                tampil = tampil + saldo
+                MsgBox("Saldo anda tidak cukup")
+            Else
+                Dim simpan As String = "INSERT INTO tbl_kas (kd_transaksi,tanggal,jenis, jml_uang, keterangan, saldo) values('" & txtKode.Text & "','" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "', '" & ComboBox1.Text & "','" & txtJumlah.Text & "','" & txtKet.Text & "','" & tampil & "') "
+                CMD = New OdbcCommand(simpan, CONN)
+                CMD.ExecuteNonQuery()
+                MsgBox("Data Berhasil di Input")
+            End If
+
+            updateDisplay()
+            TampilGrid()
+            KosongkanData()
+
+            tutupkoneksi()
+        End If
+    End Sub
+
+    Private Sub txtKode_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtKode.KeyPress
+        txtKode.MaxLength = 6
+        If e.KeyChar = Chr(13) Then
+            bukakoneksi()
+            CMD = New OdbcCommand("select * from tbl_kas where kd_transaksi= '" & txtKode.Text & "'", CONN)
+            RD = CMD.ExecuteReader
+            RD.Read()
+            If Not RD.HasRows Then
+                MsgBox("Kode transaksi tidak ada")
+                txtKode.Focus()
+            Else
+                ComboBox1.Text = RD.Item("jenis")
+                txtJumlah.Text = RD.Item("jml_uang")
+                txtKet.Text = RD.Item("keterangan")
+                DateTimePicker1.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        bukakoneksi()
+        Dim edit As String = "update tbl_kas set
+            tanggal = '" & Format(DateTimePicker1.Value, "yyyy-MM-dd") & "',
+            jenis = '" & ComboBox1.Text & "',
+            jml_uang = '" & txtJumlah.Text & "',
+            keterangan = '" & txtKet.Text & "'
+            where kd_transaksi = '" & txtKode.Text & "'"
+
+        CMD = New OdbcCommand(edit, CONN)
+        CMD.ExecuteNonQuery()
+        MsgBox("data berhasil diupdate")
+        TampilGrid()
+        updateDisplay()
+        KosongkanData()
+        tutupkoneksi()
+    End Sub
+
+    Private Sub BtnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If txtKode.Text = "" Then
+            MsgBox("Silahkan Pilih Data yang akan dihapus dengan memasukkan kode transaksi dan tekan ENTER")
+        Else
+            If MessageBox.Show("Yakin akan dihapus ?", "", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+                bukakoneksi()
+                Dim hapus As String = "delete From tbl_kas where kd_transaksi= '" & txtKode.Text & "' "
+                CMD = New OdbcCommand(hapus, CONN)
+                CMD.ExecuteNonQuery()
+                updateDisplay()
+                TampilGrid()
+                KosongkanData()
+                tutupkoneksi()
+            End If
+        End If
+    End Sub
+
+    Private Sub updateDisplay()
+        bukakoneksi()
+        CMD = New OdbcCommand(" select * from tbl_kas order by kd_transaksi desc limit 1", CONN)
+        RD = CMD.ExecuteReader
+        RD.Read()
+        If Not RD.HasRows Then
+            lbl_Saldo.Text = " Saldo Utama Rp. 0" & RD.Item("saldo")
+        Else
+            lbl_Saldo.Text = " Saldo Utama Rp. " & RD.Item("saldo")
+            tampil = RD.Item("saldo")
+        End If
+        tutupkoneksi()
+    End Sub
+End Class
